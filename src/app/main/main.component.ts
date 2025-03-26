f0b8d27e3dfd3994107dda574ace33de3a01d795
Martin Bock <martinbock@arcor.de>
2025-03-26 12:59:33 +0100

    "login and signup component added"

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, SignupComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
