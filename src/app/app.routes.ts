76adb3d813f02e24642f001d867095605b9dc886
Martin Bock <martinbock@arcor.de>
2025-03-26 12:50:33 +0100

    "main, legal and policy components created, fonts and styles added, material design added"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+// Importiere die Standalone-Komponenten
+import { MainComponent } from './main/main.component';
+import { LegalComponent } from './shared/legal/legal.component';
+import { PolicyComponent } from './shared/policy/policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'legal', component: LegalComponent },
+  { path: 'policy', component: PolicyComponent },
+];
