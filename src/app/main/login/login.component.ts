158f9bfe833fd07a8335d6573fef816e156705da
Martin Bock <martinbock@arcor.de>
2025-04-01 17:43:49 +0200

    "login component start animation created"

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  /** UI States */
+  isPageLoaded: boolean = false;
+  isLogoShifted: boolean = false;
+  isLogoVisible: boolean = false;
+  // isBackgroundReset: boolean = false;
 
+  faded = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.faded = true;      
+    }, 3000);
+
+    setTimeout(() => {
+      this.isLogoVisible = true;
+    }, 3200);
+
+    setTimeout(() => {
+      this.isLogoShifted = true;
+      this.isPageLoaded = true;
+    }, 3300);
+  }
 }
+
