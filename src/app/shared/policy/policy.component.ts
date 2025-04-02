d5217fe74d37ea5ad814d728a86192185fa868d7
Martin Bock <martinbock@arcor.de>
2025-04-02 17:05:13 +0200

    "legal notice & privacy policy styled"

--- src/app/shared/policy/policy.component.ts
+++ src/app/shared/policy/policy.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-policy',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './policy.component.html',
-  styleUrl: './policy.component.scss'
+  styleUrl: './policy.component.scss',
 })
-export class PolicyComponent {
-
-}
+export class PolicyComponent {}
