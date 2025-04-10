00437b7e3a006607f8172f8ca62a89fb62a08ef7
Martin Bock <martinbock@arcor.de>
2025-04-10 11:51:35 +0200

    "workspace menu button added, sliding effect added"

--- src/app/main/content/content.component.ts
+++ src/app/main/content/content.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss',
 })
 export class ContentComponent {
   isEditing: boolean = false;
+  isLeftVisible: boolean = true;
 
   toggleEditState(): void {
     this.isEditing = !this.isEditing; // Toggle the state between true and false
+  }
+
+  toggleLeftPanel(): void {
+    this.isLeftVisible = !this.isLeftVisible;
   }
 }
